ba7add06ba8f054ed374edbb63d64620c82a8d2b
maxmozzi <maksym.lavryk2006@gmail.com>
2025-12-26 16:54:16 +0100

    fasdfasf

--- netlify/functions/getProducts.js
+++ netlify/functions/getProducts.js
@@ -1,15 +1,23 @@
 export async function handler(event, context) {
-  const API_KEY = process.env.BILLGANG_API_KEY;
-  const SHOP_ID = process.env.BILLGANG_SHOP_ID;
+  try {
+    const API_KEY = process.env.BILLGANG_API_KEY;
+    const SHOP_ID = process.env.BILLGANG_SHOP_ID;
 
-  const res = await fetch(`https://api.billgang.com/v1/shops/${SHOP_ID}/products`, {
-    headers: { Authorization: `Bearer ${API_KEY}` }
-  });
+    const res = await fetch(`https://pg-api.billgang.com/v1/dash/shops/${SHOP_ID}/products`, {
+      headers: { Authorization: `Bearer ${API_KEY}` }
+    });
 
-  const data = await res.json();
+    const data = await res.json();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ errorMessage: "Fetch failed", errorType: error.toString() })
+    };
+  }
 }
